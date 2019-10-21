714d54183260f0b880068e07f6c9a3c722485c85
Kaliabianau, Vitali <vitali.kaliabianau@ihsmarkit.com>
2019-10-21 15:54:21 +0300

    asd

---
+++ ConsoleApp2/CodeWars_Fibonacci.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+public class Fibonacci
+{
+	public static BigInteger fib(int n)
+	{
+		if (n == 0)
+		{
+			return 0;
+		}
+
+		if (n == 1)
+		{
+			return 1;
+		}
+
+		BigInteger result = 1;
+		BigInteger prev = 0;
+
+		for (int i = 2; i <= n; i++)
+		{
+			var temp = result;
+			result = result + prev;
+			prev = temp;
+		}
+
+		return result;
+	}
+}
